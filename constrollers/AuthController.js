e709c32257c7dffbef098f6d63636449a391fbab
Sérgio Moura <smouracalmon@gmail.com>
2020-04-26 08:24:34 -0300

    Funções do index.js externalizadas para controller

---
+++ constrollers/AuthController.js
@@ -0,0 +1,18 @@
+const AuthController = {
+    
+    showLogin: (req,res) => {
+        res.render('auth/login');
+    },
+
+    showRegistro: (req,res) => {
+        res.render('auth/register');
+    },
+
+    showHome: (req,res) => {
+        res.render('index');
+    }
+
+
+}
+
+module.exports = AuthController;
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+const AuthController = require("../constrollers/AuthController");
 
-
-router.get('/', function(req, res, next) {
-  res.render('auth/login');
-});
-
-router.get('/registro', function(req, res, next) {
-  res.render('auth/register');
-});
-
-router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', AuthController.showLogin);
+router.get('/registro',AuthController.showRegistro);
+router.get('/home', AuthController.showHome);
 
 module.exports = router;
